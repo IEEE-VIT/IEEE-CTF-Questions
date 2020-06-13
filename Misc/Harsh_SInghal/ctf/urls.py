b0ba990e650e92169d801d33f701431833fb3093
Harsh <hs2002singhal@gmail.com>
2020-06-13 10:41:44 +0000

    Files

---
+++ Misc/Harsh_SInghal/ctf/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from .import views
+
+
+urlpatterns = [
+    path('black.htm', views.ctf,name='black'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('dontbesobrutal',views.zips, name="brute"),
+    path('dontbesobrutal/robot/',views.robot, name="robot"),
+
+]
