0e3d4704ac8cb763182a733cf8f8eea18f892076
hsrambo07 <hs2002singhal@gmail.com>
2020-07-21 21:44:10 +0000

    modified and added manage.py file

--- Misc/Harsh_SInghal/ctf/ctf/urls.py
+++ Misc/Harsh_SInghal/ctf/ctf/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('dontbesobrutal',views.zips, name="brute"),
     path('dontbesobrutal/robot/',views.robot, name="robot"),
+    path('dontbesobrutal/robot.txt',views.robot, name="robots"),
 
 ]
