381b0f01f966548102321f3c0e3dcf164530e96d
DarthBenro008 <hkpdev008@gmail.com>
2020-10-24 07:28:46 +0530

    fix: random

--- WEB/web-poornesh/hello/db_connect.php
+++ WEB/web-poornesh/hello/db_connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysqli_connect('localhost', 'user', 'password');
+$connection = mysqli_connect('mysql', 'user', 'password');
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'mashlog_demo');
+$select_db = mysqli_select_db($connection, 'information_schema');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 }
