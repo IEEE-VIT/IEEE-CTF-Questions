868d3bfcbef2f4c263b012015c25178281b5f4d5
Tesla369 <56352298+Tesla369@users.noreply.github.com>
2020-10-25 20:53:44 +0530

    Update seeds.rb

--- WEB/web3-poornesh/db/seeds.rb
+++ WEB/web3-poornesh/db/seeds.rb
@@ -3,5 +3,10 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+ #  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{id: 1 }, {name: "admin" }, {email: "admin@ieeevit.org" }, {secret_fact: "Passphrases are always kept secret" }, {password_digest: "$2a$12$ey7vxtDiVQAIsUmTTbYUtO2.AMivMGf1sRX2cqyAlyQKAU4.8tsmG" }])
+p "Created #{User.count} users"
+
+#users = Users.execute("INSERT INTO users (id , name, email, secret_fact, password_digest) VALUES (?, ?, ?, ?, ?)", ["1", "admin", "admin@ieeevit.org", "Passphrases are always kept secret", "$2a$12$ey7vxtDiVQAIsUmTTbYUtO2.AMivMGf1sRX2cqyAlyQKAU4.8tsmG"])
