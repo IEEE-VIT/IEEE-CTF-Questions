f05c418f5a65a45a62bc73a2d25ab48fc294cbe4
hsrambo07 <hs2002singhal@gmail.com>
2020-10-26 00:34:03 +0000

    updated logi with django auth

--- Misc/Harsh_SInghal/question/ctf/urls.py
+++ Misc/Harsh_SInghal/question/ctf/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path,include
 from .import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.login),
-    path('login/dontbesobrutal/',views.zips, name="brute"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),    path('login/dontbesobrutal/',views.zips, name="brute"),
     path('login/dontbesobrutal/robot/',views.robot, name="robot"),
     path('login/dontbesobrutal/robot.txt',views.hint, name="robot"),
     path('login/dontbesobrutal/robot/download',views.download, name="robot"),
