c4e6bab55e01de6eadf6a703cf6a3a2e1e6ebcdc
DarthBenro008 <hkpdev008@gmail.com>
2020-10-27 12:44:13 +0530

    fix: deploy typos

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker build -t aryan9600/ieeectf-nodejs-vm:latest -t aryan9600/ieeectf-nodejs-v
 
 docker build -t aryan9600/ieeectf-haunted:latest -t aryan9600/ieeectf-haunted:$SHA -f ./WEB/haunted-house/Dockerfile ./WEB/haunted-house
 
-docker build -t aryan9600/ieeectf-advertisment:latest -t aryan9600/ieeectf-advertisment:$SHA -f ./WEB/advertisment-auth/Dockerfile ./WEB/advertisment-auth
+docker build -t aryan9600/ieeectf-advertisment:latest -t aryan9600/ieeectf-advertisment-auth:$SHA -f ./WEB/advertisment-auth/Dockerfile ./WEB/advertisment-auth
 
 docker build -t aryan9600/ieeectf-ssrf:latest -t aryan9600/ieeectf-ssrf:$SHA -f ./WEB/Ajmal\ SSRF/Dockerfile ./WEB/Ajmal\ SSRF
 
@@ -21,7 +21,7 @@ docker push aryan9600/ieeectf-pwn:latest
 docker push aryan9600/ieeectf-dontbesobrutal:latest
 docker push aryan9600/ieeectf-nodejs-vm:latest
 docker push aryan9600/ieeectf-haunted:latest
-docker push aryan9600/ieeectf-advertisment:latest
+docker push aryan9600/ieeectf-advertisment-auth:latest
 docker push aryan9600/ieeectf-ssrf:latest
 docker push aryan9600/ieeectf-jail:latest
 docker push aryan9600/ieeectf-jailed:latest
@@ -32,7 +32,7 @@ docker push aryan9600/ieeectf-pwn:$SHA
 docker push aryan9600/ieeectf-dontbesobrutal:$SHA
 docker push aryan9600/ieeectf-nodejs-vm:$SHA
 docker push aryan9600/ieeectf-haunted:$SHA
-docker push aryan9600/ieeectf-advertisment:$SHA
+docker push aryan9600/ieeectf-advertisment-auth:$SHA
 docker push aryan9600/ieeectf-ssrf:$SHA
 docker push aryan9600/ieeectf-jail:$SHA
 docker push aryan9600/ieeectf-jailed:$SHA
@@ -40,14 +40,14 @@ docker push aryan9600/ieeectf-re:$SHA
 
 
 kubectl apply -f k8s
-kubectl patch configmap tcp-services -n kube-system --patch "$(cat k8s/patch.json)"
-kubectl patch deployment ingress-nginx-controller --patch "$(cat k8s/ingress-nginx-controller-patch.yaml)" -n kube-system
-kubectl set image deployments/advertisment-auth-deployment advertisment=aryan9600/ieeectf-advertisment:$SHA
+# kubectl patch configmap tcp-services -n kube-system --patch "$(cat k8s/patch.json)"
+# kubectl patch deployment ingress-nginx-controller --patch "$(cat k8s/ingress-nginx-controller-patch.yaml)" -n kube-system
+kubectl set image deployments/advertisment-auth-deployment advertisment=aryan9600/ieeectf-advertisment-auth:$SHA
 kubectl set image deployments/dontbesobrutal-deployment dbsb=aryan9600/ieeectf-dontbesobrutal:$SHA
 kubectl set image deployments/node-vm-deployment node-vm=aryan9600/ieeectf-nodejs-vm:$SHA
 kubectl set image deployments/php-deployment php-server=aryan9600/ieeectf-haunted:$SHA
 kubectl set image deployments/ssrf-deployment ssrf=aryan9600/ieeectf-ssrf:$SHA
 kubectl set image deployments/pwn-deployment pwn=aryan9600/ieeectf-pwn:$SHA
-kubectl set image deployments/re-deployment advertisment=aryan9600/ieeectf-re:$SHA
-kubectl set image deployments/jail-deployment advertisment=aryan9600/ieeectf-jail:$SHA
-kubectl set image deployments/jailed-deployment advertisment=aryan9600/ieeectf-jailed:$SHA
+kubectl set image deployments/re-deployment re=aryan9600/ieeectf-re:$SHA
+kubectl set image deployments/jail-deployment jail=aryan9600/ieeectf-jail:$SHA
+kubectl set image deployments/jailed-deployment jailed=aryan9600/ieeectf-jailed:$SHA
