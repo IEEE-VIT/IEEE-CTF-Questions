662711c0608313a515a0fec3ba5b6b15466a40c9
DarthBenro008 <hkpdev008@gmail.com>
2020-10-28 01:56:58 +0530

    fix: ruby base url

--- WEB/advertisment-auth/config.ru
+++ WEB/advertisment-auth/config.ru
@@ -2,4 +2,7 @@
 
 require_relative 'config/environment'
 
-run Rails.application
+map '/advertisment' do
+    run Rails.application
+end
+
--- WEB/haunted-house/hello/selections.php
+++ WEB/haunted-house/hello/selections.php
@@ -1,22 +1,6 @@
 <?php
 
-/*
-
-bWAPP, or a buggy web application, is a free and open source deliberately insecure web application.
-It helps security enthusiasts, developers and students to discover and to prevent web vulnerabilities.
-bWAPP covers all major known web vulnerabilities, including all risks from the OWASP Top 10 project!
-It is for security-testing and educational purposes only.
-
-Enjoy!
-
-Malik Mesellem
-Twitter: @MME_IT
-
-bWAPP is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (http://creativecommons.org/licenses/by-nc-nd/4.0/). Copyright © 2014 MME BVBA. All rights reserved.
-
-*/
-
-include("admin/settings.php");
+/*include("admin/settings.php");
 
 $bugs = file("bugs.txt");
 
@@ -33,7 +17,7 @@ if(isset($_POST["form_bug"]) && isset($_POST["bug"]))
 
     exit;
    
-}
+}*/
  
 if(isset($_POST["form_security_level"]) && isset($_POST["security_level"]))    
 {
