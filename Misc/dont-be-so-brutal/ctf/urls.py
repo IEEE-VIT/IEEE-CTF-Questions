22841211ea01e56c37b16cc4fca5f1b0226a7bbf
Sanskar Jaiswal <jaiswalsanskar078@gmail.com>
2020-10-29 20:10:17 +0000

    add force script name

--- Misc/dont-be-so-brutal/ctf/urls.py
+++ Misc/dont-be-so-brutal/ctf/urls.py
@@ -3,7 +3,8 @@ from .import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),    path('login/dontbesobrutal/',views.zips, name="brute"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/dontbesobrutal/',views.zips, name="brute"),
     path('login/dontbesobrutal/robot/',views.robot, name="robot"),
     path('login/dontbesobrutal/robot.txt',views.hint, name="robot"),
     path('login/dontbesobrutal/robot/download',views.download, name="robot"),
